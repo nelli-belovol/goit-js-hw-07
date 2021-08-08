d2a897f94d1f1639b9abd386c9f8ec983df81a93
nelli-belovol <nelli199020@gmail.com>
2021-08-08 22:20:41 +0300

    добавила 8 задание

--- homework-07/src/js/task-08.js
+++ homework-07/src/js/task-08.js
@@ -1,37 +1,45 @@
-// const divBoxes = document.querySelector('#boxes');
-// const input = document.querySelector('[type="number"]');
-// const divRender = document.querySelector('[data-action="render"]');
-// const divDestroy = document.querySelector('[data-action="destroy"]');
-// // console.dir(input);
-// // console.log(divRender);
-// // console.log(divDestroy);
-// let quantity = 0;
-// let divWidth = 30;
-// let divHeigth = 30;
+const divBoxes = document.querySelector('#boxes');
+const input = document.querySelector('[type="number"]');
+const divRender = document.querySelector('[data-action="render"]');
+const divDestroy = document.querySelector('[data-action="destroy"]');
+// console.dir(input);
+// console.log(divRender);
+// console.log(divDestroy);
+let quantity = 0;
+let divWidth = 30;
+let divHeigth = 30;
 
-// function colorGen() {
-//   let color = '';
-//   const r = Math.floor(Math.random() * 256);
-//   const g = Math.floor(Math.random() * 256);
-//   const b = Math.floor(Math.random() * 256);
-//   return (color = 'rgb(' + r + ',' + g + ',' + b + ')');
-// }
+function colorGen() {
+  let color = '';
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return (color = 'rgb(' + r + ',' + g + ',' + b + ')');
+}
 
-// input.addEventListener('change', event => {
-//   return (quantity = Number(event.target.value));
-// });
+function createBoxes(amount) {
+  const arrDivs = [];
+  for (let i = 1; i <= amount; i += 1) {
+    const div = document.createElement('div');
+    div.style.background = colorGen();
+    divWidth += 10;
+    divHeigth += 10;
+    div.style.width = `${divWidth}px`;
+    div.style.height = `${divHeigth}px`;
+    arrDivs.push(div);
+  }
+  return arrDivs;
+}
 
-// divRender.addEventListener('click', createBoxes(quantity));
+divRender.addEventListener('click', () => {
+  divBoxes.append(...createBoxes(Number(input.value)));
+  console.log(divBoxes);
+});
 
-// function createBoxes(amount) {
-//   for (let i = 1; i <= amount; i += 1) {
-//     const div = document.createElement('div');
-//     console.log(div);
-//     div.style.background = colorGen();
-//     divWidth += 10;
-//     divHeigth += 10;
-//     div.style.width = `${divWidth}px`;
-//     div.style.height = `${divHeigth}px`;
-//     divBoxes.prepend(div);
-//   }
-// }
+function destroyBoxes() {
+  divBoxes.innerHTML = '';
+}
+
+divDestroy.addEventListener('click', () => {
+  destroyBoxes();
+});
